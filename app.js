186fcdf8a3cd1f63e1e6279b292954ca0e058f95
jam <jaymee602@gmail.com>
2022-05-12 23:37:57 -0700

    add inquirer

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs)
+const inquirer = require('inquirer');
 
-const printProfileData = profileDataArr => {
-    // This...
-    for (let i = 0; i < profileDataArr.length; i += 1) {
-      console.log(profileDataArr[i]);
-    }
-  
-    console.log('================');
-  
-    // Is the same as this...
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-};
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?'
+        }
+    ])
+    .then(answers => console.log(answers));
+// const fs = require('fs');
 
+// const generatePage = require('./src/page-template.js');
 
-
-printProfileData(profileDataArgs);
+// fs.writeFile('./index.html', generatePage(name, github), err => {
+//     if (err) throw new Error(err);
+    
+//     console.log('Portfolio complete! Check out index.html to see the output!');
+// });
 
